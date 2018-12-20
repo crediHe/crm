0d30109bc61678de98e4202ff5b3ebab68997ee0
crediHe <834613167@qq.com>
2018-12-20 16:29:09 +0800

    crm客户管理系统 第13次提交 营销管理    营销机会管理 查询功能模块后端接口 事件格式优化

---
+++ src/main/java/com/shsxt/crm/dao/SaleChanceMapper.java
@@ -0,0 +1,9 @@
+package com.shsxt.crm.dao;
+
+import com.shsxt.crm.base.BaseDao;
+import com.shsxt.crm.po.SaleChance;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaleChanceMapper extends BaseDao<SaleChance> {
+}
